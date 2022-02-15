0385daf53d365205e1b29030f3563d16dfd7cb2b
Ian L <ileach81@gmail.com>
2022-02-14 19:31:42 -0500

    added index api

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -47,3 +47,10 @@ async function signupFormHandler(event) {
     }
   }
 }
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);

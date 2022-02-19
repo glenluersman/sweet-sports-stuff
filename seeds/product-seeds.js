b14d91e391d866e02598f279c70a577170059aae
glenluersman <glen.luersman@gmail.com>
2022-02-18 22:42:17 -0500

    seeds

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -4,64 +4,64 @@ const productdata = [
   {
     product_name: 'Mens Shirt',
     category_id: 1,
-    price: 50.00,
-    product_desc: 'this is a shirt',
+    price: 37.99,
+    product_desc: 'Ohio State Buckeyes Colosseum Campbell Long Sleeve Hoodie T-Shirt - Camo',
     product_image: '/images/shirts/mensShirt1.jpg'
   },
   {
     product_name: 'Mens Shirt',
     category_id: 1,
-    price: 50.00,
-    product_desc: 'this is a shirt',
+    price: 29.99,
+    product_desc: 'Ohio State Buckeyes Nike Stencil Wordmark T-Shirt - Olive',
     product_image: '/images/shirts/mensShirt2.jpg'
   },
   {
     product_name: 'Mens Shirt',
     category_id: 1,
-    price: 50.00,
-    product_desc: 'this is a shirt',
+    price: 48.00,
+    product_desc: 'Ohio State Buckeyes Nike Long Sleeve Mantra T-Shirt',
     product_image: '/images/shirts/mensShirt3.jpg'
   },
   {
     product_name: 'Womens Shirt',
     category_id: 2,
-    price: 75.00,
-    product_desc: 'this is a shirt',
+    price: 44.99,
+    product_desc: "Ohio State Buckeyes Women's Call the Shots Luxe Boyfriend Long Sleeve T-Shirt - Charcoal",
     product_image: '/images/shirts/womensShirt1.jpg'
   },
   {
     product_name: 'Womens Shirt',
     category_id: 2,
-    price: 75.00,
-    product_desc: 'this is a shirt',
+    price: 99.00,
+    product_desc: "#0 Ohio State Buckeyes Nike Women's Game Football Jersey - Scarlet",
     product_image: '/images/shirts/womensShirt2.jpg'
   },
   {
     product_name: 'Womens Shirt',
     category_id: 2,
-    price: 75.00,
-    product_desc: 'this is a shirt',
+    price: 39.99,
+    product_desc: "Ohio State Buckeyes Women's Plus Size Logo Long Sleeve T-Shirt - Heathered Gray",
     product_image: '/images/shirts/womensShirt3.jpg'
   },
   {
     product_name: 'Kids Shirt',
     category_id: 3,
-    price: 100.00,
-    product_desc: 'this is a shirt',
+    price: 49.99,
+    product_desc: '#1 Ohio State Buckeyes Nike Toddler Replica Football Jersey - Black',
     product_image: '/images/shirts/kidsShirt1.jpg'
   },
   {
     product_name: 'Kids Shirt',
     category_id: 3,
-    price: 100.00,
-    product_desc: 'this is a shirt',
+    price: 49.99,
+    product_desc: '#1 Ohio State Buckeyes Nike Toddler Replica Football Jersey - Red',
     product_image: '/images/shirts/kidsShirt2.jpg'
   },
   {
     product_name: 'Kids Shirt',
     category_id: 3,
-    price: 100.00,
-    product_desc: 'this is a shirt',
+    price: 34.99,
+    product_desc: 'Ohio State Buckeyes Colosseum Youth VF Cut Sew Pullover Hoodie - Scarlet',
     product_image: '/images/shirts/kidsShirt3.jpg'
   }
 ];
--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -2,19 +2,139 @@ const { Review } = require('../models');
 
 const reviewdata = [
   {
-    content: 'Nunc rhoncus dui vel sem.',
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
     user_id: 1,
     product_id: 1
   },
   {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 1,
+    product_id: 2
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 1,
+    product_id: 3
+  },
+  {
     content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 1,
+    product_id: 4
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 1,
+    product_id: 5
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 1,
+    product_id: 6
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 1,
+    product_id: 7
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 1,
+    product_id: 8
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 1,
+    product_id: 9
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 2,
+    product_id: 1
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
     user_id: 2,
     product_id: 2
   },
   {
-    content: 'Aliquam erat volutpat. In congue.',
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 2,
+    product_id: 3
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 2,
+    product_id: 4
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 2,
+    product_id: 5
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 2,
+    product_id: 6
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 2,
+    product_id: 7
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 2,
+    product_id: 8
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 2,
+    product_id: 9
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 3,
+    product_id: 1
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 3,
+    product_id: 2
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
     user_id: 3,
     product_id: 3
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 3,
+    product_id: 4
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 3,
+    product_id: 5
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 3,
+    product_id: 6
+  },
+  {
+    content: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+    user_id: 3,
+    product_id: 7
+  },
+  {
+    content: 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.',
+    user_id: 3,
+    product_id: 8
+  },
+  {
+    content: 'Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    user_id: 3,
+    product_id: 9
   }
 ];
 
